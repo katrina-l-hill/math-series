1e4c5cea9996ab5b7f1891581e193998a615ed84
Katrina Hill <khill365@gmail.com>
2022-04-19 16:33:04 -0700

    added function for lucas and sum_series

--- math_series/series.py
+++ math_series/series.py
@@ -3,3 +3,16 @@ from test_series import test_fibonacci
 
 def fibonacci():
     pass
+
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n-1) + lucas(n-2)
+
+print(lucas(3))
+
+def sum_series(n, first_param=0, second_param=1):
+    print(n, first_param, second_param)
