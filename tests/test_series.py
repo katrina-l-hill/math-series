bbdd66c328ecceccb26a8bd1d75a717da9e9cc7a
Katrina Hill <khill365@gmail.com>
2022-04-19 14:55:33 -0700

    defined fibonacci and tested two functions

---
+++ tests/test_series.py
@@ -0,0 +1,21 @@
+from cmath import exp
+from unittest import expectedFailure
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+def test_fib_10():
+    actual = fibonacci(10)
+    expected = 55
+    assert actual == expected
+
+def test_fib_20():
+    actual = fibonacci(20)
+    expected = 6765
+    assert actual == expected
