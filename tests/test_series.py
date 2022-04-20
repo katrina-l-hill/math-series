59ffce0b38f74139810af7c420801568268c04e8
Katrina Hill <khill365@gmail.com>
2022-04-19 21:57:21 -0700

    added tests for lucas and sum_series

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,12 +1,4 @@
-
-
-def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+from math_series.series import fibonacci, lucas, sum_series
 
 def test_fib_10():
     actual = fibonacci(10)
@@ -17,3 +9,29 @@ def test_fib_20():
     actual = fibonacci(20)
     expected = 6765
     assert actual == expected
+
+def test_lucas_10():
+    actual = lucas(10)
+    expected = 123
+    assert actual == expected
+
+def test_lucas_20():
+    actual = lucas(20)
+    expected = 15127
+    assert actual == expected
+
+def test_sum_series_fib_10():
+    actual = sum_series(10)
+    expected = 55
+    assert actual == expected
+
+def test_sum_series_lucas_10():
+    actual = sum_series(10, 2, 1)
+    expected = 123
+    assert actual == expected
+
+def test_func_equivalence_fib():
+    assert fibonacci(10) == sum_series(10)
+
+def test_func_equivalence_lucas():
+    assert lucas(10) == sum_series(10,2,1)
