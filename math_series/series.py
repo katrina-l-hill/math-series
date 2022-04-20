adb4c1b0f3cc15ed53e567adfe99329897ca56fe
Katrina Hill <khill365@gmail.com>
2022-04-19 21:55:43 -0700

    added docstrings for fibonacci, lucas, and sum_series

--- math_series/series.py
+++ math_series/series.py
@@ -1,7 +1,18 @@
-from test_series import test_fibonacci
+"""
+This function has one parameter, n. It returns the nth value in the fibonacci series. This function is implemented using recursion.
+"""
 
-def fibonacci():
-    pass
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+"""
+This function has one parameter, n. It returns the nth value in the lucas series. This function is implemented using recursion.
+"""
 
 def lucas(n):
     if n == 0:
@@ -9,9 +20,21 @@ def lucas(n):
     elif n == 1:
         return 1
     else:
-        return lucas(n-1) + lucas(n-2)
+        return lucas(n-2) + lucas(n-1)
 
-print(lucas(3))
+"""
+This function has one required parameter and two optional parameters. This function performs recursive addition based upon the inputs given.
+"""
 
 def sum_series(n, first_param=0, second_param=1):
-    print(n, first_param, second_param)
+    if n == 0:
+        return first_param
+    elif n == 1:
+        return second_param
+    else:
+        return sum_series(n-2, first_param, second_param) + sum_series(n-1, first_param, second_param)
+ 
+
+print(sum_series(10))
+
+print(sum_series(10, 2, 1))
